00185a3ebf833fa6793b711efc283c1f440a4e28
Andres Melendez <codeslator@gmail.com>
2022-04-08 14:38:46 -0400

    Initializing router config

---
+++ src/config/router/router.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const Router: FC = () => (
+  <BrowserRouter>
+    <Routes>
+
+    </Routes>
+  </BrowserRouter>
+);
